570f378145034c77983a31183dea3c34e42e2e67
DavidHavl <info@davidhavl.com>
2024-07-11 14:02:34 +0200

    Add Env to db middleware

--- src/middleware/db.middleware.ts
+++ src/middleware/db.middleware.ts
@@ -1,3 +1,4 @@
+import type { Env } from '@/types';
 import { drizzle } from 'drizzle-orm/d1';
 import type { Context } from 'hono';
 
@@ -5,10 +6,16 @@ export let db = null;
 /**
  * Middleware for injecting db to context
  */
-export const dbMiddleware = async (c: Context, next) => {
+export const dbMiddleware = async (c: Context<Env>, next) => {
+  if (c.env.DB === undefined) {
+    throw new Error('DB is not defined');
+  }
+
   if (db === null) {
     db = drizzle(c.env.DB, { logger: true });
   }
+
   c.set('db', db);
+
   return next();
 };
